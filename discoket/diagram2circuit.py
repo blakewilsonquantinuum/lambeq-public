a06aaf72bdf8203940132f0fcb6e4a1fd000a7db
Ian Fan <ian.fan@cambridgequantum.com>
2021-07-07 13:29:29 +0100

    Neaten `diagram2circuit.py`

--- discoket/diagram2circuit.py
+++ discoket/diagram2circuit.py
@@ -5,97 +5,80 @@ An ansatz is used to convert a DisCoCat diagram into a quantum circuit.
 
 """
 
-from abc import ABC, abstractmethod
-from typing import Any, Dict, List
-
-from discopy.rigid import Box, Diagram, Ty
-from discopy.quantum import Bra, Ket
-from discopy.quantum import Circuit as DiscopyCircuit
-from discopy.quantum import IQPansatz as IQP
-from discopy.quantum.circuit import Functor, Id
-import numpy as np
-from pytket import Circuit as TketCircuit
-from sympy.abc import symbols
-from sympy.core.symbol import Symbol
-
 __all__ = ['Ansatz', 'IQPAnsatz']
 
+from abc import ABC, abstractmethod
+from typing import Any, List, Mapping
 
-def _summarise_box(box: Box) -> str:
-    """Summarise the given discopy box."""
-    dom = str(box.dom).replace(' @ ', '@')
-    cod = str(box.cod).replace(' @ ', '@')
-    args = box.name, dom, cod
-    if any([' ' in arg for arg in args]):
-        exc = 'Name and types of boxes should not contain whitespace'
-        raise ValueError(exc)
-
-    return '_'.join(args)
-
-
-def _make_symbols(label: str, n_params: int) -> List[Symbol]:
-    """Generate a list of symbols for a given label."""
-    return [symbols(f"{label}_{i}") for i in range(n_params)]
+import discopy
+from discopy.quantum.circuit import Functor, Id, IQPansatz as IQP
+from discopy.quantum.gates import Bra, Ket
+from discopy.rigid import Box, Diagram, Ty
+import numpy as np
+import pytket
+from sympy.core.symbol import Symbol
 
 
 class Ansatz(ABC):
     """Base class for circuit ansatz."""
 
-    @abstractmethod
-    def __init__(self, ob_map: Dict[Ty, int], **kwargs: Any) -> None:
+    def __init__(self, ob_map: Mapping[Ty, int], **kwargs: Any) -> None:
         """Instantiates a circuit ansatz.
 
         Parameters
         ----------
         ob_map : dict
-            A dictionary which maps `discopy.rigid.Ty` into an integer, the
-            number of qubits it has in circuit form.
-        kwargs : dict
+            A mapping from `discopy.rigid.Ty` to the number of qubits it
+            uses in a circuit.
+        **kwargs : dict
             Extra parameters for ansatz configuration.
 
         """
+        self.ob_map = ob_map
 
-    def __call__(self, diagram: Diagram) -> DiscopyCircuit:
-        """Convert the given discopy diagram into a discopy circuit."""
-        return self._functor(diagram)
+    @abstractmethod
+    def diagram2circuit(self, diagram: Diagram) -> discopy.Circuit:
+        """Convert a DisCoPy diagram into a DisCoPy circuit."""
 
-    def diagram2tket(self, diagram: Diagram) -> TketCircuit:
-        """Convert the given discopy diagram into a tket circuit."""
-        return self(diagram).to_tk()
+    def diagram2tket(self, diagram: Diagram) -> pytket.Circuit:
+        """Convert a DisCoPy diagram into a tket circuit."""
+        return self.diagram2circuit(diagram).to_tk()
 
     def _arity_from_type(self, pg_type: Ty) -> int:
-        """Calculate number of circuit qubits for the given pregroup type."""
-        return sum(self._ob[Ty(factor.name)] for factor in pg_type)
+        """Calculate the number of qubits used for a given type."""
+        return sum(self.ob_map[Ty(factor.name)] for factor in pg_type)
 
 
 class IQPAnsatz(Ansatz):
-    """Class which implements the IQP ansatz.
+    """Instantaneous Quantum Polynomial anastz.
 
-    A Instantaneous Quantum Polynomial (IQP) anastz involves interleaving
-    layers of Hadamrd gates with diagonal unitaries. This class uses n-1
-    adjacent CRz gates to implement each diagonal unitary.
+    An IQP anastz interleaves layers of Hadamard gates with diagonal
+    unitaries. This class uses `n-1` adjacent CRz gates to implement
+    each diagonal unitary.
 
     """
 
-    def __init__(self, ob_map: Dict[Ty, int], n_layers: int) -> None:
+    def __init__(self, ob_map: Mapping[Ty, int], n_layers: int) -> None:
         """Instantiates an IQP ansatz.
 
         Parameters
         ----------
         ob_map : dict
-            A dictionary which maps `discopy.rigid.Ty` into an integer, the
-            number of qubits it has in circuit form.
+            A mapping from `discopy.rigid.Ty` to the number of qubits it
+            uses in a circuit.
         n_layers : int
             The number of IQP layers used by the ansatz.
 
         """
-        self._ob = ob_map
+        super().__init__(ob_map=ob_map, n_layers=n_layers)
         self.n_layers = n_layers
+        self.functor = Functor(ob=self.ob_map, ar=self._ar)
 
-        self._functor = Functor(ob=self._ob, ar=self._ar)
+    def diagram2circuit(self, diagram: Diagram) -> discopy.Circuit:
+        return self.functor(diagram)
 
-    def _ar(self, box: Box) -> DiscopyCircuit:
-        label = _summarise_box(box)
+    def _ar(self, box: Box) -> discopy.Circuit:
+        label = self._summarise_box(box)
         dom_arity = self._arity_from_type(box.dom)
         cod_arity = self._arity_from_type(box.cod)
 
@@ -106,10 +89,10 @@ class IQPAnsatz(Ansatz):
             circuit = Id()
         elif n_qubits == 1:
             # Rx(v0) >> Rz(v1) >> Rx(v2)
-            circuit = IQP(1, _make_symbols(label, 3))
+            circuit = IQP(1, self._make_symbols(label, 3))
         else:
             n_params = n_layers * (n_qubits-1)
-            syms = _make_symbols(label, n_params)
+            syms = self._make_symbols(label, n_params)
             params = np.array(syms).reshape((n_layers, n_qubits-1))
             circuit = IQP(n_qubits, params)
 
@@ -118,3 +101,20 @@ class IQPAnsatz(Ansatz):
         else:
             circuit <<= Id(dom_arity) @ Ket(*[0]*(cod_arity - dom_arity))
         return circuit
+
+    @staticmethod
+    def _make_symbols(label: str, n_params: int) -> List[Symbol]:
+        """Generate a list of symbols for a given label."""
+        return [Symbol(f'{label}_{i}') for i in range(n_params)]
+
+    @staticmethod
+    def _summarise_box(box: Box) -> str:
+        """Summarise the given discopy box."""
+        dom = str(box.dom).replace(' @ ', '@')
+        cod = str(box.cod).replace(' @ ', '@')
+        ret = '_'.join((box.name, dom, cod))
+        if ' ' in ret:
+            raise ValueError('Name and types of boxes must not contain '
+                             f'whitespace (got box with name={box.name},'
+                             f' dom={dom}, cod={cod})')
+        return ret
--- tests/test_diagram2circuit.py
+++ tests/test_diagram2circuit.py
@@ -1,9 +1,10 @@
-from discopy import Box, Cup, Diagram, Ty, Word
+import pytest
+
+from discopy import Box, Cup, Ty, Word
 from discopy.quantum import Bra, CRz, CX, H, Ket, Rx, Rz, sqrt
 from discopy.quantum.circuit import Id
 from discoket.core.types import AtomicType
-from discoket.diagram2circuit import IQPAnsatz, _summarise_box
-import pytest
+from discoket.diagram2circuit import IQPAnsatz
 from pytket.qasm import circuit_to_qasm_str
 from sympy.abc import symbols as sym
 
@@ -12,75 +13,58 @@ S = AtomicType.SENTENCE
 
 
 def test_iqp_ansatz():
-    d = Diagram(
-        dom=Ty(),
-        cod=S,
-        boxes=[
-            Word('Alice', N),
-            Word('runs', N.r @ S),
-            Cup(N, N.r)],
-        offsets=[0, 1, 0]
-    )
-
+    diagram = (Word('Alice', N) @ Word('runs', N >> S) >>
+               Cup(N, N.r) @ Id(S))
     ansatz = IQPAnsatz({N: 1, S: 1}, n_layers=1)
 
-    discopy_circuit = ansatz(d)
-    tket_circuit = ansatz.diagram2tket(d)
+    expected_circuit = (Ket(0) >>
+                        Rx(sym('Alice_Ty()_n_0')) >>
+                        Rz(sym('Alice_Ty()_n_1')) >>
+                        Rx(sym('Alice_Ty()_n_2')) >>
+                        Id(1) @ Ket(0, 0) >> Id(1) @ H @ Id(1) >>
+                        Id(2) @ H >>
+                        Id(1) @ CRz(sym('runs_Ty()_n.r@s_0')) >>
+                        CX @ Id(1) >>
+                        H @ Id(2) >>
+                        Id(1) @ sqrt(2) @ Id(2) >>
+                        Bra(0, 0) @ Id(1))
+    assert ansatz.diagram2circuit(diagram) == expected_circuit
 
-    expected_circuit = (
-        Ket(0) >> Rx(sym('Alice_Ty()_n_0')) >> Rz(sym('Alice_Ty()_n_1')) >>
-        Rx(sym('Alice_Ty()_n_2')) >> Id(1) @ Ket(0, 0) >> Id(1) @ H @ Id(1) >>
-        Id(2) @ H >> Id(1) @ CRz(sym('runs_Ty()_n.r@s_0')) >> CX @ Id(1) >>
-        H @ Id(2) >> Id(1) @ sqrt(2) @ Id(2) >> Bra(0, 0) @ Id(1)
-    )
+    expected_qasm = """OPENQASM 2.0;
+include "qelib1.inc";
 
-    expected_qasm = (
-        'OPENQASM 2.0;\n'
-        'include "qelib1.inc";\n'
-        '\n'
-        'qreg q[3];\n'
-        'creg c[2];\n'
-        'rx((2*Alice_Ty()_n_0)*pi) q[0];\n'
-        'h q[1];\n'
-        'h q[2];\n'
-        'rz((2*Alice_Ty()_n_1)*pi) q[0];\n'
-        'crz((2*runs_Ty()_n.r@s_0)*pi) q[1],q[2];\n'
-        'rx((2*Alice_Ty()_n_2)*pi) q[0];\n'
-        'cx q[0],q[1];\n'
-        'measure q[1] -> c[1];\n'
-        'h q[0];\n'
-        'measure q[0] -> c[0];\n'
-    )
+qreg q[3];
+creg c[2];
+rx((2*Alice_Ty()_n_0)*pi) q[0];
+h q[1];
+h q[2];
+rz((2*Alice_Ty()_n_1)*pi) q[0];
+crz((2*runs_Ty()_n.r@s_0)*pi) q[1],q[2];
+rx((2*Alice_Ty()_n_2)*pi) q[0];
+cx q[0],q[1];
+measure q[1] -> c[1];
+h q[0];
+measure q[0] -> c[0];
+"""
 
-    assert discopy_circuit == expected_circuit
-    assert circuit_to_qasm_str(tket_circuit) == expected_qasm
+    assert circuit_to_qasm_str(ansatz.diagram2tket(diagram)) == expected_qasm
 
 
 def test_bad_summary():
     d = Word("no spaces allowed", S)
     ansatz = IQPAnsatz({N: 0, S: 1}, n_layers=1)
     with pytest.raises(ValueError):
-        ansatz(d)
+        ansatz.diagram2circuit(d)
 
 
 def test_iqp_ansatz_inverted():
     d = Box("inverted", S, Ty())
     ansatz = IQPAnsatz({N: 0, S: 0}, n_layers=1)
-
-    assert ansatz(d) == Bra()
+    assert ansatz.diagram2circuit(d) == Bra()
 
 
 def test_iqp_ansatz_empty():
-    d = Diagram(
-        dom=Ty(),
-        cod=S,
-        boxes=[
-            Word('Alice', N),
-            Word('runs', N.r @ S),
-            Cup(N, N.r)],
-        offsets=[0, 1, 0]
-    )
-
-    from discopy.quantum.circuit import Id
+    diagram = (Word('Alice', N) @ Word('runs', N >> S) >>
+               Cup(N, N.r) @ Id(S))
     ansatz = IQPAnsatz({N: 0, S: 0}, n_layers=1)
-    assert ansatz(d) == Bra() >> Bra()
+    assert ansatz.diagram2circuit(diagram) == Bra() >> Bra()
