fd8188631bbc4af23aa09da0c08a7c1abec3c11d
y-richie-y <richie.yeung1@gmail.com>
2021-10-05 17:26:41 +0100

    Show inherited methods also add _static path to docs

--- docs/conf.py
+++ docs/conf.py
@@ -36,10 +36,10 @@ extensions = [
 ]
 
 autodoc_default_options = {
-    "members": True,
-    "undoc-members": True,
-    'show-inheritence': False,
-    "special-members": '__init__, __call__',
+    'members': True,
+    'inherited-members': True,
+    'undoc-members': True,
+    'special-members': '__init__, __call__',
 }
 
 # This disables the need to document methods in the class docstring.
@@ -53,7 +53,6 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
@@ -64,4 +63,15 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
+html_static_path = ['_static']
+
+
+def autodoc_skip_member(app, what, name, obj, skip, options):
+    if name == 'Symbol':
+        options['inherited-members'] = False
+        return False
+    return skip
+
+
+def setup(app):
+    app.connect('autodoc-skip-member', autodoc_skip_member)
